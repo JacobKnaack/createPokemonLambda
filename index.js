cfdd04be7458e76f42240b7155724382b336af5b
JacobKnaack <jacobknck@gmail.com>
2023-04-26 15:16:13 -0700

    adds dynamoose

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const dynamoose = require('dynamoose');
+
+const schema = new dynamoose.Schema({
+  name: String,
+  type: String,
+  weakness: String
+});
+
+const pokemonModel = dynamoose.model('Pokemon', schema);
+
 exports.handler = async (event) => {
   // TODO implement
   console.log(event.body);
+  let requestBody = null;
+  if (event.body) {
+    requestBody = JSON.parse(event.body);
+  }
+
+  const pokemonValues = await pokemonModel.create(requestBody);
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(pokemonValues),
   };
   return response;
 };
